b1a89b951b7cb3b370a08935eb41c77f758448b8
Joshua Musiol <joshua.musiol@web.de>
2021-05-23 14:08:34 +0200

    outlines roughly the introduction text

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
 export default function App() {
     return (
-        <>
-            <header className="header-small">
-                <h1>Portfolio</h1>
-            </header>
-            <div>hola</div>
-
-            <footer>2021</footer>
-        </>
+        <div className="mainWrapper">
+            <p className="introduction">
+                I am a Junior Fullstack Web Developer. With a background in
+                systemic organizational consulting, I have a solution-oriented
+                mindset that is complemented by my curiosity and empathetic
+                perspective as anthropologist.
+            </p>
+        </div>
     );
 }
