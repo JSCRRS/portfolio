d21512d1a084c787b25b4996151cde0bf6b57a69
Joshua Musiol <joshua.musiol@web.de>
2021-05-23 22:17:46 +0200

    copies email to clipboard

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,9 @@
 export default function App() {
+    function copyEmail() {
+        const email = "myEmail";
+        navigator.clipboard.writeText(email);
+    }
+
     return (
         <div className="main-wrapper">
             <div className="person">
@@ -9,7 +14,9 @@ export default function App() {
                     curiosity and empathetic perspective as anthropologist.
                 </p>
                 <div className="person-info">
-                    <button className="info-button">email</button>
+                    <button className="info-button" onClick={copyEmail}>
+                        email
+                    </button>
                     <button className="info-button">linkedIn</button>
                     <button className="info-button">github</button>
                     <button className="info-button">cv</button>
