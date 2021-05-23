d01cac938f1bd3e234ffb2c00514fd670741e5c2
Joshua Musiol <joshua.musiol@web.de>
2021-05-23 16:35:25 +0200

    displays sketch of buttons

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,20 @@
 export default function App() {
     return (
-        <div className="mainWrapper">
-            <p className="introduction">
-                I am a Junior Fullstack Web Developer. With a background in
-                systemic organizational consulting, I have a solution-oriented
-                mindset that is complemented by my curiosity and empathetic
-                perspective as anthropologist.
-            </p>
+        <div className="main-wrapper">
+            <div className="person">
+                <p className="introduction">
+                    I am a Junior Fullstack Web Developer. With a background in
+                    systemic organizational consulting, I have a
+                    solution-oriented mindset that is complemented by my
+                    curiosity and empathetic perspective as anthropologist.
+                </p>
+                <div className="person-info">
+                    <button className="info-button">email</button>
+                    <button className="info-button">linkedIn</button>
+                    <button className="info-button">github</button>
+                    <button className="info-button">cv</button>
+                </div>
+            </div>
         </div>
     );
 }
