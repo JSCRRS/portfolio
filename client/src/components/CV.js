985f032f6078e6ec480d62b23458c70dd5fb5a94
Joshua Musiol <joshua.musiol@web.de>
2021-05-25 16:16:56 +0200

    renders first section of cv

--- client/src/components/CV.js
+++ client/src/components/CV.js
@@ -1,3 +1,19 @@
 export default function CV() {
-    return <div>my CV is here</div>;
+    return (
+        <div className="cv-wrapper">
+            <div className="cv-header">
+                <div className="profile-wrapper">
+                    <div className="profile-pic">
+                        <img src="/profile.jpg" alt="profile" />
+                    </div>
+                </div>
+                <div className="profile-details">
+                    <div className="profile-name">
+                        <h4>Joshua Musiol</h4>
+                    </div>
+                    <div>Junior Fullstack Developer in Berlin</div>
+                </div>
+            </div>
+        </div>
+    );
 }
