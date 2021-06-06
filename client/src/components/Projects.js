c6bb3f95c5cf6872459d01c828eb6bf2842a3f0b
Joshua Musiol <joshua.musiol@web.de>
2021-06-06 19:24:39 +0200

    shows four project templates

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -35,6 +35,40 @@ export default function Projects() {
                     <button className="project-button">view</button>
                 </div>
             </div>
+            <div className="project-wrapper">
+                <img
+                    className="project-img"
+                    src="/profile.jpg"
+                    alt="project logo"
+                ></img>
+                <div className="project-details">
+                    <div className="project-text">
+                        <p className="project-title">Title</p>
+                        <p className="project-description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do ei
+                        </p>
+                    </div>
+                    <button className="project-button">view</button>
+                </div>
+            </div>
+            <div className="project-wrapper">
+                <img
+                    className="project-img"
+                    src="/profile.jpg"
+                    alt="project logo"
+                ></img>
+                <div className="project-details">
+                    <div className="project-text">
+                        <p className="project-title">Title</p>
+                        <p className="project-description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do ei
+                        </p>
+                    </div>
+                    <button className="project-button">view</button>
+                </div>
+            </div>
         </>
     );
 }
